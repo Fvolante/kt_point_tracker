0ccdd147ba9cb30ff96ce0ed44bf918d72b89909
Francesco Lamacchia <lamacchia.volante@gmail.com>
2023-06-27 18:37:58 +0200

    complete victory points logic

--- script.js
+++ script.js
@@ -18,16 +18,22 @@ const removeHomeTacOps = document.getElementById("remove-home-tac-ops");
 const addHomeMission = document.getElementById("add-home-mission");
 const removeHomeMission = document.getElementById("remove-home-mission");
 const addGuestTacOps = document.getElementById("add-guest-tac-ops");
-const removeGuestTacOps = document.getElementById("add-guest-tac-ops");
+const removeGuestTacOps = document.getElementById("remove-guest-tac-ops");
 const addGuestMission = document.getElementById("add-guest-mission");
-const removeGuestMission = document.getElementById("remove-guest-mission"); 
+const removeGuestMission = document.getElementById("remove-guest-mission");
+const operativeListHome = document.getElementById("operative-list-home");
+const operativeListGuest = document.getElementById("operative-list-guest"); 
 
 // Operatives lists
 const addOperativeHomeBtn = document.getElementById("add-operative-home");
 const addOperativeGuestBtn = document.getElementById("add-operative-guest");
 
+//console.log(operativeListHome.children);
+let childList = operativeListHome.children;
+console.log(childList);
 
-
+let childArr = Array.from(childList);
+console.log(childArr);
 
 
 /* MANAGE VICTORY POINTS */
@@ -44,31 +50,58 @@ function updatePoints(pointsAdded, el) {
 /* 
 update total points UI
 */
-function updateTotal() {
+function updateTotalHome() {
     let tacOps = Number(homeTacOpsPoints.innerText);
     let mission = Number(homeMissionPoints.innerText);
 
     homeTotPoints.innerText = tacOps + mission;
 }
 
+function updateTotalGuest() {
+    let tacOps = Number(guestTacOpsPoints.innerText);
+    let mission = Number(guestMissionPoints.innerText);
+
+    guestTotPoints.innerText = tacOps + mission;
+}
+
 addHomeTacOps.addEventListener("click", function() {
     updatePoints(1, homeTacOpsPoints);
-    updateTotal();
+    updateTotalHome();
+})
+
+addGuestTacOps.addEventListener("click", function() {
+    updatePoints(1, guestTacOpsPoints);
+    updateTotalGuest();
 })
 
 removeHomeTacOps.addEventListener("click", function() {
     updatePoints(-1, homeTacOpsPoints);
-    updateTotal();
+    updateTotalHome();
+})
+
+removeGuestTacOps.addEventListener("click", function() {
+    updatePoints(-1, guestTacOpsPoints);
+    updateTotalGuest();
 })
 
 addHomeMission.addEventListener("click", function() {
     updatePoints(1, homeMissionPoints);
-    updateTotal();
+    updateTotalHome();
+})
+
+addGuestMission.addEventListener("click", function() {
+    updatePoints(1, guestMissionPoints);
+    updateTotalGuest();
 })
 
 removeHomeMission.addEventListener("click", function() {
     updatePoints(-1, homeMissionPoints);
-    updateTotal();
+    updateTotalHome();
+})
+
+removeGuestMission.addEventListener("click", function() {
+    updatePoints(-1, guestMissionPoints);
+    updateTotalGuest();
 })
 
 
